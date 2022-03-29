7aa1d42717399b3c9098b363a6d8a54a2f301af8
josh Long <joshlong@joshs-MacBook-Pro.local>
2022-03-29 15:42:28 -0400

    dev: further decoupling of authentication steps for library impl

--- packages/sonr-webauthn/src/webauthn.ts
+++ packages/sonr-webauthn/src/webauthn.ts
@@ -1,6 +1,6 @@
 import { CredentialOptions } from "./credentials";
 import { CreateSessionState, GetSessionState } from "./state";
-import { bufferDecode } from "./utils";
+import { bufferDecode, bufferEncode } from "./utils";
 
 CreateSessionState();
 const state: any = GetSessionState();
@@ -50,39 +50,39 @@ function makeCredential(name: string) {
     var user_verification = $('#select-verification').find(':selected').val();
     var resident_key_requirement = $('#select-residency').find(':selected').val();
     var txAuthSimple_extension = $('#extension-input').val();
-
-    fetch('/makeCredential/' + state.user.name, { 
-        method: "POST",
-        body: JSON.stringify({
-                attType: attestation_type,
-                authType: authenticator_attachment,
-                userVerification: user_verification,
-                residentKeyRequirement: resident_key_requirement,
-                txAuthExtension: txAuthSimple_extension,
-            })
-        }).then((makeCredentialOptions: Response) => {
-            makeCredentialOptions.publicKey.challenge = bufferDecode(makeCredentialOptions.publicKey.challenge);
-            makeCredentialOptions.publicKey.user.id = bufferDecode(makeCredentialOptions.publicKey.user.id);
-            if (makeCredentialOptions.publicKey.excludeCredentials) {
-                for (var i = 0; i < makeCredentialOptions.publicKey.excludeCredentials.length; i++) {
-                    makeCredentialOptions.publicKey.excludeCredentials[i].id = bufferDecode(makeCredentialOptions.publicKey.excludeCredentials[i].id);
+    checkUserExists().then(function() {
+        fetch('/makeCredential/' + state.user.name, { 
+            method: "POST",
+            body: JSON.stringify({
+                    attType: attestation_type,
+                    authType: authenticator_attachment,
+                    userVerification: user_verification,
+                    residentKeyRequirement: resident_key_requirement,
+                    txAuthExtension: txAuthSimple_extension,
+                })
+            }).then((makeCredentialOptions: any) => {
+                makeCredentialOptions.publicKey.challenge = bufferDecode(makeCredentialOptions.publicKey.challenge);
+                makeCredentialOptions.publicKey.user.id = bufferDecode(makeCredentialOptions.publicKey.user.id);
+                if (makeCredentialOptions.publicKey.excludeCredentials) {
+                    for (var i = 0; i < makeCredentialOptions.publicKey.excludeCredentials.length; i++) {
+                        makeCredentialOptions.publicKey.excludeCredentials[i].id = bufferDecode(makeCredentialOptions.publicKey.excludeCredentials[i].id);
+                    }
                 }
-            }
-            console.log("Credential Creation Options");
-        });
+                console.log(`Credential Creation Options: ${makeCredentialOptions}`);
+            });
+    });
 }
 
 // This should be used to verify the auth data with the server
-function registerNewCredential(newCredential) {
+function registerNewCredential(newCredential: any) {
     // Move data into Arrays incase it is super long
     let attestationObject = new Uint8Array(newCredential.response.attestationObject);
     let clientDataJSON = new Uint8Array(newCredential.response.clientDataJSON);
     let rawId = new Uint8Array(newCredential.rawId);
 
-    $.ajax({
-        url: '/makeCredential',
-        type: 'POST',
-        data: JSON.stringify({
+    fetch('/makeCredential', {
+        method: 'POST',
+        body: JSON.stringify({
             id: newCredential.id,
             rawId: bufferEncode(rawId),
             type: newCredential.type,
@@ -90,68 +90,38 @@ function registerNewCredential(newCredential) {
                 attestationObject: bufferEncode(attestationObject),
                 clientDataJSON: bufferEncode(clientDataJSON),
             },
-        }),
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function(response) {
-            $("#login-button").popover('show')
-        }
-    });
-}
-
-function addUserErrorMsg(msg) {
-    if (msg === "username") {
-        msg = 'Please correct your SNR name.';
-    } else {
-        msg = 'Please remove .snr/';
-    }
-    document.getElementById("user-create-error").innerHTML = msg;
-}
-
-function getAssertion() {
-    hideErrorAlert();
-    if ($("#input-snr-name").val() === "") {
-        showErrorAlert("Please correct your SNR name.");
-        return;
-    }
-    setUser();
-    $.get('/user/' + state.user.name + '/exists', {}, null, 'json').done(function(response) {
-            console.log(response);
-        }).then(function() {
-
-            var user_verification = $('#select-verification').find(':selected').val();
-            var txAuthSimple_extension = $('#extension-input').val();
-
-            $.get('/assertion/' + state.user.name, {
-                    userVer: user_verification,
-                    txAuthExtension: txAuthSimple_extension
-                }, null, 'json')
-                .done(function(makeAssertionOptions) {
-                    console.log("Assertion Options:");
-                    console.log(makeAssertionOptions);
-                    makeAssertionOptions.publicKey.challenge = bufferDecode(makeAssertionOptions.publicKey.challenge);
-                    makeAssertionOptions.publicKey.allowCredentials.forEach(function(listItem) {
-                        listItem.id = bufferDecode(listItem.id)
-                    });
-                    console.log(makeAssertionOptions);
-                    navigator.credentials.get({
-                            publicKey: makeAssertionOptions.publicKey
-                        })
-                        .then(function(credential) {
-                            console.log(credential);
-                            verifyAssertion(credential);
-                        }).catch(function(err) {
-                            console.log(err.name);
-                            showErrorAlert(err.message);
-                        });
-                });
         })
-        .catch(function(error) {
-            if (!error.exists) {
-                showErrorAlert("User not found, try registering one first!");
-            }
-            return;
+    });
+};
+
+
+function getAssertion(userVerification: string, txAuthSimple: string, name: string) {
+    new Promise((resolve, reject) => {
+        state.user.name = name;
+        fetch('/assertion/' + state.user.name, {
+            body: JSON.stringify({
+                userVer: userVerification,
+                txAuthExtension: txAuthSimple
+            // TODO: Create type for asstertion response
+            })
+        }).then(function(makeAssertionOptions: any) {
+            console.log(makeAssertionOptions);
+            makeAssertionOptions.publicKey.challenge = bufferDecode(makeAssertionOptions.publicKey.challenge);
+            makeAssertionOptions.publicKey.allowCredentials.forEach(function(listItem) {
+                listItem.id = bufferDecode(listItem.id)
+            });
+            console.log(makeAssertionOptions);
+            
+            navigator.credentials.get({
+                publicKey: makeAssertionOptions.publicKey
+            }).then(function(credential) {
+                console.log(credential);
+                verifyAssertion(credential);
+            }).catch(function(err) {
+                console.log(err.name);
+            });
         });
+    });
 }
 
 function verifyAssertion(assertedCredential) {

788b407a1fd0f25c58420bf849d71a591d1bd8ec
josh Long <joshlong@joshs-MacBook-Pro.local>
2022-03-29 15:41:46 -0400

    dev: addition of create and get credential implementation

--- packages/sonr-webauthn/src/credentials.ts
+++ packages/sonr-webauthn/src/credentials.ts
@@ -1,3 +1,74 @@
-export type CredentialOptions {
-    publicKey: Credential,
-}
+import { BrowserSupport } from "./enums";
+import { detectWebAuthnSupport } from "./utils";
+
+/*
+
+*/
+function createCredentialOptions(serverContext: string, userName: string, displayName: string): PublicKeyCredentialCreationOptions {
+    return {
+        challenge: Uint8Array.from(
+            serverContext, c => c.charCodeAt(0)),
+        rp: {
+            name: "Duo Security",
+            id: "duosecurity.com",
+        },
+        user: {
+            id: Uint8Array.from(
+                "UZSL85T9AFC", c => c.charCodeAt(0)),
+            name: "lee@webauthn.guide",
+            displayName: "Lee",
+        },
+        // -7 indicates that the server accepts Elliptic Curve public keys using a SHA-256
+        pubKeyCredParams: [{alg: -7, type: "public-key"}],
+        authenticatorSelection: {
+            authenticatorAttachment: "cross-platform",
+        },
+        timeout: 60000,
+        attestation: "direct"
+    };    
+};
+
+/*
+
+*/
+export async function createCredentials(
+    options: PublicKeyCredentialCreationOptions
+    ): Promise<Credential | null> {
+    try
+    {
+        const browserSupport: BrowserSupport = detectWebAuthnSupport();
+        if (browserSupport == BrowserSupport.NonHttps || BrowserSupport.Unsupported)
+            throw new Error("Browser does not support credentials");
+        
+        const cred: Credential = await navigator.credentials.create({
+            publicKey: options
+        });
+
+        return cred;
+    } catch(e)
+    {
+        console.error(`Error while creating public key credentials ${e.message}`);
+        throw e; // throw error back to the caller to handle.
+    }
+};
+
+/*
+
+*/
+export async function getCredentials(pk: PublicKeyCredentialRequestOptions): Promise<Credential | null> {
+    try
+    {
+        const browserSupport: BrowserSupport = detectWebAuthnSupport();
+        if (browserSupport == BrowserSupport.NonHttps || BrowserSupport.Unsupported)
+            throw new Error("Browser does not support credentials");
+        const credResponse = await navigator.credentials.get({
+            publicKey: pk
+        });
+        
+        return credResponse;
+    } catch(e)
+    {
+        console.error(`Error while getting public key credentials ${e.message}`);
+        throw e; // throw error back to the caller to handle.
+    }
+};

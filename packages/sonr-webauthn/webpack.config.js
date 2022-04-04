f14a37f369ff88414432a133ba7b5a999db6e558
josh long <josh@sonr.io>
2022-04-04 17:21:13 -0400

    dev: addition of enviorments to webpack bundle

--- packages/sonr-webauthn/webpack.config.js
+++ packages/sonr-webauthn/webpack.config.js
@@ -1,34 +1,39 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.ts',
-  externals: {
-    'react': { // import react from an external module so you don't have multiple instances
-        'commonjs': 'react', 
-        'amd': 'react'
-     },
-    'react-dom': { // some versions of react had links to react-dom so its good to include this
-        'commonjs': 'react-dom',
-        'amd': 'react-dom'
-    }
-  },
-  target: 'web',
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+module.exports = (env) => {
+  const isProd = env.production ? true : false;
+  console.log(`env found to be ${isProd}`);
+
+  return {
+    entry: './src/index.ts',
+    externals: {
+      'react': { // import react from an external module so you don't have multiple instances
+          'commonjs': 'react', 
+          'amd': 'react'
       },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    libraryTarget: 'umd',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+      'react-dom': { // some versions of react had links to react-dom so its good to include this
+          'commonjs': 'react-dom',
+          'amd': 'react-dom'
+      }
+    },
+    target: 'web',
+    mode: isProd ? 'production' : 'development',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'bundle.js',
+      libraryTarget: 'umd',
+      path: path.resolve(__dirname, 'dist'),
+    }
+  }
+}

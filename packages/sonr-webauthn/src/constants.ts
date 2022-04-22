25dadcafa856c776302bc2db7032aa5d6b8a7445
josh long <josh@sonr.io>
2022-04-22 17:19:01 -0500

    update to endpoints for new api schema

--- packages/sonr-webauthn/src/constants.ts
+++ packages/sonr-webauthn/src/constants.ts
@@ -1,6 +1,6 @@
 export const storageKey = 'sonr-username';
-export const makeCredentialsEndpoint = 'v1/name/register/start';
+export const makeCredentialsEndpoint = '/v1/name/register/start';
 export const assertionEndpoint = '/v1/name/register/finish';
 
-export const verifyAssertionEndpoint = 'v1/name/access/start';
-export const authenticateUserEndpoint = 'v1/name/access/finish/';
+export const verifyAssertionEndpoint = '/v1/name/access/start';
+export const authenticateUserEndpoint = '/v1/name/access/finish/';

167d23df8c85cf4c78ceae67a14fb47a352ffa2b
josh long <josh@sonr.io>
2022-06-27 10:23:17 -0400

    config updates

--- jest.config.js
+++ jest.config.js
@@ -3,20 +3,21 @@ module.exports = async () => {
         bail: 3,
         clearMocks: true,
         collectCoverageFrom: [
-            "packages/sonr-webauthn/*/**.{js,jsx,ts,tsx}"
+            "packages/**/src/**.{js,jsx,ts,tsx}"
         ],
         coverageThreshold: {
             global: {
               lines: 4,
             },
         },
-        coverageReporters: ["json", "lcov", "text"],
-        coverageDirectory: "./coverage",
+        collectCoverage: true,
+        mapCoverage: true,
         verbose: true,
         moduleFileExtensions: ['js', 'ts', 'tsx'],
         preset: "ts-jest",
         transform: {
             '^.+\\.ts?$': 'ts-jest'
-        }
+        },
+        testEnvironment: "jsdom"
     };
 };

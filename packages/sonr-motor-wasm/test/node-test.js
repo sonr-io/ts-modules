3a92c1e96e9fa8b6d9eb0ec0e453aa4200de00ba
josh long <josh@sonr.io>
2022-07-01 10:19:02 -0400

    addition of test env for node runtime

---
+++ packages/sonr-motor-wasm/test/node-test.js
@@ -0,0 +1,16 @@
+require('./../src/polyfis');
+require('./../lib/wasm_exec');
+const path = require('path');
+
+const fs = require('fs');
+
+const wasmBuffer = fs.readFileSync(path.join(__dirname, '../lib/sonr-motor.wasm'));
+const runtime = new Go()
+
+WebAssembly.instantiate(wasmBuffer, runtime.importObject).then(async lib => {
+  // Exported function live under instance.exports
+  runtime.run(lib.instance);
+}).then(() => {
+    console.log("module initialized invoking methods")
+    globalThis.createWallet();
+});
